98c2647c485807ecfad8f7704faa487ca3de63ca
alexherron <alexeherron@gmail.com>
2023-11-09 10:22:43 -0600

    React frontend mostly complete, working on backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Header />
         <main className='py-3'>
           <Container>
-            <h1>Buy My Stuff</h1>
+            <Outlet />
           </Container>
         </main>
         <Footer />
